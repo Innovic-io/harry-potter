709a2c4a41aa9ed33be9763e9fe15073bd3d92a7
Milana <milana.bijeljanin@gmail.com>
2020-02-26 14:23:43 +0100

    Search

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  @Input() title: string;
+  @Input() house;
 
   constructor() { }
 
