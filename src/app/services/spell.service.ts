b1627fc509f51697837bcfc44557252f2d968975
Kristina <ilic.kristina1996@gmail.com>
2020-02-28 09:51:02 +0100

    white icons, list of spells

--- src/app/services/spell.service.ts
+++ src/app/services/spell.service.ts
@@ -5,12 +5,13 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class SpellService {
-
+  private readonly apiRoot = 'https://www.potterapi.com/v1/';
+  private readonly token = '$2a$10$uO8ooOUh7hPLnBF/HVyPgOHA3FptZxvqGr916lQl9L8RMWVWYOA3O';
   constructor(private http: HttpClient) {
   }
 
   getSpell() {
-    return this.http.get('https://www.potterapi.com/v1/spells?key=$2a$10$uO8ooOUh7hPLnBF/HVyPgOHA3FptZxvqGr916lQl9L8RMWVWYOA3O');
+    return this.http.get(`${this.apiRoot}spells?key=${this.token}`);
   }
 }
 
