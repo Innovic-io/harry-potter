71f4edaba56ac8e2d24156898121b69130df84f5
Milana <milana.bijeljanin@gmail.com>
2020-03-02 10:11:10 +0100

    Badges #2

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { ICharacters } from '../../models/characters';
 import { CharacterService } from '../../services/character.service';
 
